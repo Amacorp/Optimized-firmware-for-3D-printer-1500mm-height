772b946c91f93c0ccd8c8a40f98488fabf4c57f5
Mehdi Ghahramanian <37830907+Amacorp@users.noreply.github.com>
2018-06-18 15:45:00 +0300

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -975,7 +975,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 1105.25 // Distance between the nozzle to printbed after homing
+#define MANUAL_Z_HOME_POS 1500.25 // Distance between the nozzle to printbed after homing. please don't forget about nozel distance and heatbed.
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
